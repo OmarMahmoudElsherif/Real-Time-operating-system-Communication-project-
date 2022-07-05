0f9554e928c5827b3600c73970f017d96ceeee33
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-07-05 15:42:31 +0200

    Add files via upload

---
+++ Code/Debug/system/src/stm32f4-hal/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../system/src/stm32f4-hal/stm32f4xx_hal.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_cortex.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_flash.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_flash_ex.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_flash_ramfunc.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_gpio.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_iwdg.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_pwr.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_rcc.c 
+
+OBJS += \
+./system/src/stm32f4-hal/stm32f4xx_hal.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_cortex.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash_ex.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash_ramfunc.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_gpio.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_iwdg.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_pwr.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_rcc.o 
+
+C_DEPS += \
+./system/src/stm32f4-hal/stm32f4xx_hal.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_cortex.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash_ex.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash_ramfunc.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_gpio.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_iwdg.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_pwr.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_rcc.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/stm32f4-hal/%.o: ../system/src/stm32f4-hal/%.c system/src/stm32f4-hal/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\portable\GCC\ARM_CM4F" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-unused-parameter -Wno-conversion -Wno-sign-conversion -Wno-bad-function-cast -Wno-unused-variable -Wno-implicit-function-declaration -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
