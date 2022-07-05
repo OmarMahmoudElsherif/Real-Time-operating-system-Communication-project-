03346f0cac1156ddf913e933da032be7088dad62
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-07-05 15:38:14 +0200

    Add files via upload

---
+++ Code/Debug/system/src/newlib/subdir.mk
@@ -0,0 +1,57 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../system/src/newlib/assert.c \
+../system/src/newlib/exit.c \
+../system/src/newlib/sbrk.c \
+../system/src/newlib/startup.c \
+../system/src/newlib/syscalls.c 
+
+CPP_SRCS += \
+../system/src/newlib/cxx.cpp 
+
+OBJS += \
+./system/src/newlib/assert.o \
+./system/src/newlib/cxx.o \
+./system/src/newlib/exit.o \
+./system/src/newlib/sbrk.o \
+./system/src/newlib/startup.o \
+./system/src/newlib/syscalls.o 
+
+C_DEPS += \
+./system/src/newlib/assert.d \
+./system/src/newlib/exit.d \
+./system/src/newlib/sbrk.d \
+./system/src/newlib/startup.d \
+./system/src/newlib/syscalls.d 
+
+CPP_DEPS += \
+./system/src/newlib/cxx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/newlib/%.o: ../system/src/newlib/%.c system/src/newlib/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\portable\GCC\ARM_CM4F" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+system/src/newlib/%.o: ../system/src/newlib/%.cpp system/src/newlib/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C++ Compiler'
+	arm-none-eabi-g++ -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu++11 -fabi-version=0 -fno-exceptions -fno-rtti -fno-use-cxa-atexit -fno-threadsafe-statics -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+system/src/newlib/startup.o: ../system/src/newlib/startup.c system/src/newlib/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -DOS_INCLUDE_STARTUP_INIT_MULTIPLE_RAM_SECTIONS -I"../include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\portable\GCC\ARM_CM4F" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
