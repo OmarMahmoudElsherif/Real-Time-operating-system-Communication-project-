fc1082e216d20dd8c0e9924a6c836bdae624deb2
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-07-05 15:28:37 +0200

    Add files via upload

---
+++ Code/Debug/system/src/cortexm/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../system/src/cortexm/exception-handlers.c \
+../system/src/cortexm/initialize-hardware.c \
+../system/src/cortexm/reset-hardware.c 
+
+OBJS += \
+./system/src/cortexm/exception-handlers.o \
+./system/src/cortexm/initialize-hardware.o \
+./system/src/cortexm/reset-hardware.o 
+
+C_DEPS += \
+./system/src/cortexm/exception-handlers.d \
+./system/src/cortexm/initialize-hardware.d \
+./system/src/cortexm/reset-hardware.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/cortexm/%.o: ../system/src/cortexm/%.c system/src/cortexm/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DOS_USE_SEMIHOSTING -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\include" -I"C:\Users\LAP TECH\Desktop\RTOS  project\Demo\FreeRTOS Source\portable\GCC\ARM_CM4F" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
